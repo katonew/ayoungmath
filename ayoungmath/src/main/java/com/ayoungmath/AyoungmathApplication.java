ed90bcea4244438793b2dc111297ef9ad7db3943
katonew <katonew@nton.co.kr>
2024-01-17 15:09:18 +0900

    test

---
+++ ayoungmath/src/main/java/com/ayoungmath/AyoungmathApplication.java
@@ -0,0 +1,13 @@
+package com.ayoungmath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyoungmathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyoungmathApplication.class, args);
+	}
+
+}
